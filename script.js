21abad73b0248495176afb553f3cc050c2c3a921
GrimCreeperyt <grimcreeperyt@gmail.com>
2023-12-19 09:31:54 -0500

    added some more to the second story

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let adjective1 = prompt('Enter an adjective');
 let adjective2 = prompt('Enter an adjective');
+let adjective3 = prompt('Enter an adjective');
 
 let noun1 = prompt('Enter a noun');
 let noun2 = prompt('Enter a noun');
@@ -7,6 +8,8 @@ let noun3 = prompt('Enter a noun');
 let noun4 = prompt('Enter a noun');
 let noun5 = prompt('Enter a noun');
 let noun6 = prompt('Enter a noun');
+
+let properNoun1 = prompt('Enter a proper noun');
 
 let verb1 = prompt('Enter a verb');
 let verb2 = prompt('Enter a verb');
@@ -23,6 +26,10 @@ let distance1 = prompt('Enter a unit of measuring distance');
 
 let building1 = prompt('Enter a building');
 
+let group1 = prompt('Enter a group name');
+
+let material1 = prompt('Enter a type of material')
+
 let randNum = 2;
 let story = ``;
 if (randNum === 1) {
@@ -31,7 +38,7 @@ if (randNum === 1) {
     story = `The ${title1} stood on the ${adjective1} ${noun1} outside his chambers. It was here the ${noun2} 
     came, after long flight. Their ${noun3} speckled the ${noun1} that rose ${number1} ${distance1} tall on 
     either side of him, a ${noun4} and a ${noun5}, ${number2} of the ${number3} that brooded over the ${noun6} 
-    of the ${adjective2} ${building1}. When first he came to Dragonstone, the army of stone grotesques had 
+    of the ${adjective2} ${building1}. When first he came to ${noun7}, the ${group1} of ${material1} grotesques had 
     made him uneasy, but as the years passed he had grown used to them. Now he thought of 
     them as old friends. The three of them watched the sky together with foreboding.`;
 } else if (randNum === 3) {
